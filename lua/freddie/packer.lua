b004e09d0d4a89ffb00013e1f9f0e84fe92d60ee
Your Name <you@example.com>
2024-06-01 19:56:06 +0200

    Added peek.nvim, requires installed deno

--- lua/freddie/packer.lua
+++ lua/freddie/packer.lua
@@ -65,4 +65,14 @@ return require('packer').startup(function(use)
     use("github/copilot.vim")
     use("eandrju/cellular-automaton.nvim")
     use("laytan/cloak.nvim")
+
+    use {
+  "toppair/peek.nvim",
+  build = "deno task --quiet build:fast",
+  config = function()
+    require("peek").setup()
+    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+  end,
+}
 end)
